ddbe2dbcaf1f9420c25fc488f4fdb96b24ec6685
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-28 19:31:54 +0300

    Реализуем второй подход: переход происходит неявно, страницу инициализируем в теле теста: 1. Закомментируем строку с возвращаемым значением 2. Инициализируем LoginPage в теле теста

--- final_selenium_python_testing/test_main_page.py
+++ final_selenium_python_testing/test_main_page.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -9,7 +10,7 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    login_page = page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
 

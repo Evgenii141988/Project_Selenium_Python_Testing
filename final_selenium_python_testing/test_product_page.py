d826aac88da81322b6abdeb921f2b69cd24ac9d1
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-29 16:24:30 +0300

    Добавлены два метода add_to_basket и solve_quiz_and_get_code в test_guest_can_add_product_to_basket

--- final_selenium_python_testing/test_product_page.py
+++ final_selenium_python_testing/test_product_page.py
@@ -7,7 +7,9 @@ link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handb
 def test_guest_can_add_product_to_basket(browser):
     product_page = ProductPage(browser, link)
     product_page.open()
-    
+    product_page.add_to_basket()
+    product_page.solve_quiz_and_get_code()
+
 
 if __name__ == '__main__':
     pytest.main()

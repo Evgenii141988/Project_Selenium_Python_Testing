dcf546191cd8234f8685eb890303dbb3c2fd3bcf
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-29 22:53:30 +0300

    1. В тест test_guest_can_add_product_to_basket добавлены методы проверки check_message_product_in_basket,check_message_product_price_in_basket ; 2. Добавлена параметризация с несколькими ссылками для проверки на наличие бага

--- final_selenium_python_testing/test_product_page.py
+++ final_selenium_python_testing/test_product_page.py
@@ -3,21 +3,25 @@ from .pages.product_page import ProductPage
 
 link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 
-
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
     product_page.solve_quiz_and_get_code()
-
-
-def test_guest_check_message_product_in_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209'
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_to_basket()
     product_page.check_message_product_in_basket()
     product_page.check_message_product_price_in_basket()
+
+
 
 
 if __name__ == '__main__':

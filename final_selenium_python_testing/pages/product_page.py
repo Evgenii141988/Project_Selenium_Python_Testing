cf9f6593acc801a10509f861a1e33685498497af
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-29 18:13:16 +0300

    1. Добавлены новые локаторы PRODUCT_NAME_IN_BASKET PRODUCT_PRICE_IN_BASKET изменен PRODUCT_NAME 2. Реализованы два метода check_message_product_in_basket, check_message_product_price_in_basket для проверки товара в корзине и цены в корзине 3. в тест для проверки добавлены методы из п.2

--- final_selenium_python_testing/pages/product_page.py
+++ final_selenium_python_testing/pages/product_page.py
@@ -6,3 +6,13 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         button_add_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_BASKET)
         button_add_basket.click()
+
+    def check_message_product_in_basket(self):
+        name_product = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        name_product_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_BASKET)
+        assert name_product.text == name_product_in_basket.text, 'Product name is not in basket'
+
+    def check_message_product_price_in_basket(self):
+        price_product = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        price_product_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_BASKET)
+        assert price_product.text == price_product_in_basket.text, 'Product price is not in basket'
--- final_selenium_python_testing/test_product_page.py
+++ final_selenium_python_testing/test_product_page.py
@@ -12,8 +12,12 @@ def test_guest_can_add_product_to_basket(browser):
 
 
 def test_guest_check_message_product_in_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209'
     product_page = ProductPage(browser, link)
     product_page.open()
+    product_page.add_to_basket()
+    product_page.check_message_product_in_basket()
+    product_page.check_message_product_price_in_basket()
 
 
 if __name__ == '__main__':

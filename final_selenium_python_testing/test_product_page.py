76439fc8206b4efe40bf9496f0db0db87a0c8419
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-29 15:18:09 +0300

    Добавлен тест test_guest_can_add_product_to_basket

--- final_selenium_python_testing/test_product_page.py
+++ final_selenium_python_testing/test_product_page.py
@@ -1,6 +1,13 @@
 import pytest
+from .pages.product_page import ProductPage
 
 link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+
+
+def test_guest_can_add_product_to_basket(browser):
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    
 
 if __name__ == '__main__':
     pytest.main()

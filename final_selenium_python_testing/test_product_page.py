436d42cfa5b11435cb74613576b366b297d70f2a
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-30 18:01:51 +0300

    Добавлен тест для проверки test_guest_cant_see_success_message: 1. Открываем страницу товара 2. Проверяем, что нет сообщения об успехе с помощью is_not_element_present

--- final_selenium_python_testing/test_product_page.py
+++ final_selenium_python_testing/test_product_page.py
@@ -33,6 +33,12 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page.should_not_be_success_message()
 
 
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/'
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
 
 if __name__ == '__main__':
     pytest.main()

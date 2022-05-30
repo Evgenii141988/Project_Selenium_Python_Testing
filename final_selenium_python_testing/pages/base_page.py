b626b5fabec40358e41033382d289c15a447f498
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-30 16:11:19 +0300

    Добавлен в BasePage абстрактный метод, который проверяет, что элемент не появляется на странице в течение заданного времени

--- final_selenium_python_testing/pages/base_page.py
+++ final_selenium_python_testing/pages/base_page.py
@@ -1,5 +1,8 @@
-from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 import math
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -16,6 +19,14 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert

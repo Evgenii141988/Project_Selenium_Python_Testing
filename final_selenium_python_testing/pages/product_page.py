632419c97479cb931a5e31748e1bb27ca9ec2f6c
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-30 17:19:44 +0300

    Реализован метод-проверка success_message_should_disappeared

--- final_selenium_python_testing/pages/product_page.py
+++ final_selenium_python_testing/pages/product_page.py
@@ -20,3 +20,7 @@ class ProductPage(BasePage):
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
+
+    def success_message_should_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not disappeared"

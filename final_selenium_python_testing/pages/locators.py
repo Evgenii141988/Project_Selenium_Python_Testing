77f57510bbf1b82db8ad0fea61ed5df7b1830322
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-31 20:37:09 +0300

    Добавлен локатор BASKET_LINK

--- final_selenium_python_testing/pages/locators.py
+++ final_selenium_python_testing/pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, '//span[@class="btn-group"]/a')
 
 
 class LoginPageLocators():

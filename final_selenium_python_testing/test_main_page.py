eb78fa6f315a87e14b919321692957e3cbdfdc5c
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-01 14:43:17 +0300

    В тесте test_guest_cant_see_product_in_basket_opened_from_main_page реализованы проверки: 3. в корзине нет товаров 4. есть текст о том что корзина пуста

--- final_selenium_python_testing/test_main_page.py
+++ final_selenium_python_testing/test_main_page.py
@@ -2,6 +2,7 @@ import pytest
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -24,6 +25,9 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_product_in_basket()
+    basket_page.should_basket_be_empty()
 
 
 if __name__ == '__main__':

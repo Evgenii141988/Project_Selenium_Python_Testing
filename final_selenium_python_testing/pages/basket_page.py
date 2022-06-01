66d5ba7cdbe674b382a09defce7bb1e1a5be2012
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-01 14:16:25 +0300

    В класс BasketPage добавлены два метода для проверки наличия товаров в корзине и их отсутствия

--- final_selenium_python_testing/pages/basket_page.py
+++ final_selenium_python_testing/pages/basket_page.py
@@ -1,5 +1,10 @@
 from final_selenium_python_testing.pages.base_page import BasePage
+from final_selenium_python_testing.pages.locators import BasketPageLocators
 
 
 class BasketPage(BasePage):
+    def should_not_be_product_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.PRODUCT_IN_BASKET), 'There are items in the basket'
 
+    def should_basket_be_empty(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET), 'The basket is not empty'

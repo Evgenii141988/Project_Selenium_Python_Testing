8bc94ec031735a94552c1bac9cda3f15afefda6d
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-01 17:44:30 +0300

    Добавлен в LoginPage метод register_new_user(email, password), который принимает две строки и регистрирует пользователя. Описаны соответствующие элементы страницы.

--- final_selenium_python_testing/pages/login_page.py
+++ final_selenium_python_testing/pages/login_page.py
@@ -19,3 +19,13 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), 'Registration form is not presented'
+
+    def register_new_user(self, email, password):
+        email_input = self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL)
+        email_input.send_keys(email)
+        password_input = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD)
+        password_input.send_keys(password)
+        confirm_password_input = self.browser.find_element(*LoginPageLocators.REGISTRATION_CONFIRM_PASSWORD)
+        confirm_password_input.send_keys(password)
+        registration_button = self.browser.find_element(*LoginPageLocators.REGISTRATION_BUTTON)
+        registration_button.click()

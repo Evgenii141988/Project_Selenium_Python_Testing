11540121c983a21207baf24fec5180659ec111e6
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-02 14:47:08 +0300

    add browser firefox

--- final_selenium_python_testing/conftest.py
+++ final_selenium_python_testing/conftest.py
@@ -6,13 +6,22 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
                      help='Choose user languages: en/ru/es.....')
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope='function')
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption('language')
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    browser = None
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     yield browser
     browser.quit()
